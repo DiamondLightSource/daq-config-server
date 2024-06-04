e8727e81d04c1d10bbec5a65e22d1ca388d94465
David Perl <david.perl@diamond.ac.uk>
2024-06-04 12:53:27 +0100

    separate app from main

--- src/config_service/__main__.py
+++ src/config_service/__main__.py
@@ -1,47 +1,18 @@
 from argparse import ArgumentParser
 
-from fastapi import dependencies
-
 from . import __version__
-from .constants import ENDPOINTS
 
 try:
-    import redis
-    import uvicorn
-    from dodal.beamlines.beamline_parameters import (
-        BEAMLINE_PARAMETER_PATHS,
-        GDABeamlineParameters,
-    )
-    from fastapi import FastAPI
-
-    app = FastAPI()
-    valkey = redis.Redis(host="localhost", port=6379, decode_responses=True)
+    import redis  # noqa
+    import uvicorn  # noqa
+    from fastapi import FastAPI  # noqa
 
     server_dependencies_exist = True
 except ImportError:
     server_dependencies_exist = False
 
+
 __all__ = ["main"]
-
-BEAMLINE_PARAM_PATH = ""
-BEAMLINE_PARAMS: GDABeamlineParameters | None = None
-
-
-@app.get(ENDPOINTS.BL_PARAM + "{item_id}")
-def beamlineparameter(item_id: str):
-    assert BEAMLINE_PARAMS is not None
-    return {item_id: BEAMLINE_PARAMS.params.get(item_id)}
-
-
-@app.post(ENDPOINTS.FEATURE + "{item_id}")
-def set_featureflag(item_id: str, value: bool):
-    return {"success": valkey.set(item_id, int(value))}
-
-
-@app.get(ENDPOINTS.FEATURE + "{item_id}")
-def get_featureflag(item_id: str):
-    ret = int(valkey.get(item_id))  # type: ignore
-    return {item_id: bool(ret) if ret is not None else None, "raw": ret}
 
 
 def main():
@@ -57,12 +28,10 @@ def main():
             "importing the client, you must install this package with [server] "
             "optional dependencies"
         )
-    if args.dev:
-        BEAMLINE_PARAM_PATH = "tests/test_data/beamline_parameters.txt"
     else:
-        BEAMLINE_PARAM_PATH = BEAMLINE_PARAMETER_PATHS["i03"]
-    BEAMLINE_PARAMS = GDABeamlineParameters.from_file(BEAMLINE_PARAM_PATH)
-    uvicorn.run(app="config_service.__main__:app", host="0.0.0.0", port=8555)
+        from .app import main
+
+        main(args)
 
 
 # test with: python -m config_service
