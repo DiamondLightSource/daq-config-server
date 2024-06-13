4af3a4e2980398d4e61db670b8fe303f05c00b46
David Perl <david.perl@diamond.ac.uk>
2024-06-13 09:26:39 +0100

    improve client fn arg names

--- src/daq_config_server/client.py
+++ src/daq_config_server/client.py
@@ -14,37 +14,37 @@ class ConfigServer:
         self.port = port
         self._log = log if log else getLogger("daq_config_server.client")
 
-    def _get(self, endpoint: str, param: str | None = None):
+    def _get(self, endpoint: str, item: str | None = None):
         conn = HTTPConnection(self.address, self.port)
         conn.connect()
-        conn.request("GET", endpoint + (f"/{param}" if param else ""))
+        conn.request("GET", endpoint + (f"/{item}" if item else ""))
         resp = conn.getresponse()
         assert resp.status == 200, f"Failed to get response: {resp}"
         body = json.loads(resp.read())
-        assert param in body, f"Malformed response: {body} does not contain {param}"
+        assert item in body, f"Malformed response: {body} does not contain {item}"
         resp.close()
         conn.close()
-        return body[param]
+        return body[item]
 
     def get_beamline_param(self, param: str) -> str | int | float | bool | None:
         return self._get(ENDPOINTS.BL_PARAM, param)
 
-    def get_feature_flag(self, param: str) -> bool | None:
+    def get_feature_flag(self, flag_name: str) -> bool | None:
         """Get the specified feature flag; returns None if it does not exist. Will check
         that the HTTP response is correct and raise an AssertionError if not."""
-        return self._get(ENDPOINTS.FEATURE, param)
+        return self._get(ENDPOINTS.FEATURE, flag_name)
 
     def get_feature_flag_list(self) -> list[str]:
         """Get the specified feature flag; returns None if it does not exist. Will check
         that the HTTP response is correct and raise an AssertionError if not."""
         return self._get(ENDPOINTS.FEATURE)
 
-    def best_effort_get_feature_flag(self, param: str, fallback: T = None) -> bool | T:
+    def best_effort_get_feature_flag(self, flag_name: str, fallback: T = None) -> bool | T:
         """Get the specified feature flag, returns fallback value (default None) if it
         doesn't exist or if there is a connection error - in the latter case logs
         to error."""
         try:
-            return self._get(ENDPOINTS.FEATURE, param)
+            return self._get(ENDPOINTS.FEATURE, flag_name)
         except (AssertionError, OSError):
             self._log.error(
                 "Encountered an error reading from the config service.", exc_info=True
