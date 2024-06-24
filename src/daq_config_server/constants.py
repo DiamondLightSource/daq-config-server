15c6e01879169325b680dde3c6165bad884aac33
David Perl <david.perl@diamond.ac.uk>
2024-06-24 16:30:46 +0100

    Add test

--- src/daq_config_server/constants.py
+++ src/daq_config_server/constants.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class Endpoints:
     FEATURE = "/featureflag"
+    FEATURE_DISALLOWED_SPECIALS = ["all"]
     BL_PARAM = "/beamlineparameters"
     INFO = "/info"
 
