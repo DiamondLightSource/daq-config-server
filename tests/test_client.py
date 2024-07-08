69e9e83098b5f54a02665d78f42bd67a267ab5e6
David Perl <david.perl@diamond.ac.uk>
2024-07-08 15:00:21 +0100

    add test for besteffort fetch

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,6 +4,7 @@ import requests
 from daq_config_server.client import ConfigServer
 
 SERVER_ADDRESS = "https://daq-config.diamond.ac.uk/api"
+USE_PANDA_FLAG = "use_panda_for_gridscan"
 
 
 @pytest.fixture
@@ -14,16 +15,22 @@ def server():
 @pytest.mark.uses_live_server
 class TestConfigServerClient:
     def test_fetch_one_flag_from_server(self, server: ConfigServer):
-        assert isinstance(server.get_feature_flag("use_panda_for_gridscan"), bool)
+        assert isinstance(server.get_feature_flag(USE_PANDA_FLAG), bool)
 
     def test_fetch_all_flags_from_server(self, server: ConfigServer):
         assert isinstance(flags := server.get_feature_flag_list(), list)
         assert all(isinstance(server.get_feature_flag(flag), bool) for flag in flags)
 
+    def test_best_effort_gets_real_flag(self, server: ConfigServer):
+        assert isinstance(current := server.get_feature_flag(USE_PANDA_FLAG), bool)
+        assert (
+            server.best_effort_get_feature_flag(USE_PANDA_FLAG, not current) is current
+        )
+
     @pytest.mark.skip(reason="ONLY RUN THIS MANUALLY")
     def test_fetch_and_set_flags(self, server: ConfigServer):
         """ONLY RUN THIS IF YOU ARE SURE NOTHING IS RUNNING USING THE SERVICE!!!"""
-        flag = "use_panda_for_gridscan"
+        flag = USE_PANDA_FLAG
         assert isinstance(initial_value := server.get_feature_flag(flag), bool)
         r = requests.put(
             SERVER_ADDRESS
