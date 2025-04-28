77179b10b3bb3af6cf41637eaf9b16b70a96c66f
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2025-04-28 16:25:05 +0000

    Remove unused features

--- gui/config-server-gui/build_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-module load node
-npm run build
-podman build -t daq-config-server-gui .
-podman push daq-config-server-gui gcr.io/diamond-privreg/daq-config-server/daq-config-server-gui
