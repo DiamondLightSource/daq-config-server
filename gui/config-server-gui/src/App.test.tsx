4b0a2e2e8d395381645a653ec31109181738d1be
olliesilvester <122091460+olliesilvester@users.noreply.github.com>
2025-05-08 15:39:45 +0100

    Remove unused features (#75)
    * Remove unused features
    
    * Changes to CI
    
    * Update readme
    
    Co-authored-by: Joseph Ware <53935796+DiamondJoseph@users.noreply.github.com>
    
    * Use default ingress and add section in values.yaml
    
    ---------
    
    Co-authored-by: Joseph Ware <53935796+DiamondJoseph@users.noreply.github.com>

--- gui/config-server-gui/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { screen } from "@testing-library/react";
-import { App } from "./App";
-import { render } from "./test-utils";
-
-test("title is displayed", () => {
-  render(<App />);
-  const titleText = screen.getByText(/Read and store configuration for I03 Hyperion/i);
-  expect(titleText).toBeInTheDocument();
-});
