1c57c4daac0f791ec05b1decdf7f9b5076b1646b
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2025-05-29 15:30:12 +0000

    added tag and correct logics

--- deployment/build_and_push.sh
+++ deployment/build_and_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEV=0
+TAG="dev"
 PUSH=0
 RUN_CONTAINER=0
 REBUILD_CONTAINER=0
@@ -16,32 +16,38 @@ fi
 cp .gitignore .dockerignore
 for option in "$@"; do
     case $option in
-        -d|--dev)
-            DEV=1
+        -t|--tag)
+            TAG="$2"
+            shift
+            echo "$TAG"
             ;;
         -p|--push)
             PUSH=1
+            shift
             echo "Push the container to GCR."
             ;;
         -r|--run)
+            shift
             echo "Run the container after building it."
             RUN_CONTAINER=1
             ;;
         -b|--rebuild)
+            shift
             echo "Rebuild the container even if it already exists."
             REBUILD_CONTAINER=1
 
             ;;
-        --help|--info|--h)
-            echo "Build and push the current repository state into containers and publish them to"
-            echo "gcr.io/diamond-privreg/daq-config-server/<container-name> ready for deployment."
+        --help|--info|-h)
+            echo "Build the current repository state as a container and optionally publish them to"
+            echo "gcr.io/diamond-privreg/daq-config-server/<container-name>."
             echo " "
-            echo "  -d, --dev       Creates -dev:latest tagged containers for testing on argus."
-            echo "  -p, --push      Push the container to GCR."
-            echo "  -r, --run       Run the container after building it."
-            echo "  -b, --rebuild   Rebuild the container even if it already exists."
-            echo "  --help, --info, -h  Show this help message."    
+            echo "  -t, --tag <tag>       Specify the tag for the container. Default is 'dev'."
+            echo "  -p, --push            Push the container to GCR."
+            echo "  -r, --run             Run the container after building it."
+            echo "  -b, --rebuild         Rebuild the container even if it already exists."
+            echo "  -h, --help, --info    Show this help message."    
             echo " "
+            shift
             exit 0
             ;;
 
@@ -55,23 +61,15 @@ done
 # set container and tag names:
 BASE_CONTAINER_NAME="daq-config-server"
 BASE_REPO_ADDR="gcr.io/diamond-privreg/daq-config-server/"
-if [ $DEV -gt 0 ]; then
-    echo "Building dev-mode containers..."
-    MAIN_CONTAINER_NAME="${BASE_CONTAINER_NAME}-dev"
-else
-    echo "Building prod-mode containers..."
-    MAIN_CONTAINER_NAME="${BASE_CONTAINER_NAME}"
-fi
+
+echo "Building prod-mode containers..."
+MAIN_CONTAINER_NAME="${BASE_CONTAINER_NAME}"
+
 echo " "
 
-MAIN_CONTAINER_TAG="${BASE_REPO_ADDR}${MAIN_CONTAINER_NAME}"
+MAIN_CONTAINER_TAG="${BASE_REPO_ADDR}${MAIN_CONTAINER_NAME}-${TAG}"
 
 # set env vars which will be used by build process:
-if [ $DEV -gt 0 ]; then
-    export REACT_APP_BACKEND_ADDR="http://localhost:8555"
-else
-    export REACT_APP_BACKEND_ADDR="https://daq-config.diamond.ac.uk/api"
-fi
 if [ -z "$(podman images -q $MAIN_CONTAINER_NAME 2> /dev/null)" ] || [ $REBUILD_CONTAINER -gt 0 ]; then
     echo " "
     echo "========================================="
@@ -92,7 +90,7 @@ fi
 if [ $RUN_CONTAINER -gt 0 ]; then
     echo "Running container ${MAIN_CONTAINER_NAME}..."
     # if the container is already running, stop it first
-    if podman ps -q --filter "name=$MAIN_CONTAINER_NAME" > /dev/null; then
+    if [ -n "$(podman ps -q --filter "name=$MAIN_CONTAINER_NAME")" ] ; then
         echo "Container $MAIN_CONTAINER_NAME is already running, stopping it first..."
         podman stop $MAIN_CONTAINER_NAME
     fi
