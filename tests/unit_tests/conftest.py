46427ff5d5fd1df20990ed5e814f5fa3d3bd0a3a
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2025-06-05 12:56:27 +0000

    Fix tests

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,37 +1,30 @@
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 
 import pytest
 from pytest import FixtureRequest
 
+from daq_config_server.testing._utils import make_test_response
 from daq_config_server.whitelist import get_whitelist
-from tests.constants import (
-    WHITELIST_PATH,
-)
+from tests.constants import TEST_WHITELIST_RESPONSE
 
 
 @pytest.fixture(autouse=True)
 def test_friendly_whitelist(request: FixtureRequest):
     # Don't launch threads unless we really need to since it slows down testing
 
-    if request.node.get_closest_marker("use_threading"):  # type: ignore
-        with patch("daq_config_server.whitelist.WHITELIST_REFRESH_RATE_S", new=0):
-            yield
-            get_whitelist().stop()
+    with patch("daq_config_server.whitelist.requests.get") as mock_request:
+        mock_request.return_value = make_test_response(content=TEST_WHITELIST_RESPONSE)
 
-    else:
-
-        def fake_whitelist_get():
-            response = MagicMock()
-            response.text = WHITELIST_PATH.read_text()
-            return response
-
-        with (
-            patch("daq_config_server.whitelist.Thread"),
-            patch("daq_config_server.whitelist.WhitelistFetcher.stop"),
-            patch("daq_config_server.whitelist.requests.get") as fake_get,
-        ):
-            fake_get.return_value = fake_whitelist_get()
-            yield
+        if request.node.get_closest_marker("use_threading"):  # type: ignore
+            with patch("daq_config_server.whitelist.WHITELIST_REFRESH_RATE_S", new=0):
+                yield
+                get_whitelist().stop()
+        else:
+            with (
+                patch("daq_config_server.whitelist.Thread"),
+                patch("daq_config_server.whitelist.WhitelistFetcher.stop"),
+            ):
+                yield
 
     from daq_config_server import whitelist
 
