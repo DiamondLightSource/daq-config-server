09a1d25900d2f3b74126a87cc16da55cadf1f71f
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2025-06-30 13:28:19 +0000

    Don't have separate server dependencies

--- tests/unit_tests/test_main.py
+++ tests/unit_tests/test_main.py
@@ -6,9 +6,7 @@ from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 from daq_config_server.__main__ import (
-    INSUFFICIENT_DEPENDENCIES_MESSAGE,
     __version__,
-    main,
 )
 from daq_config_server.app import log_request_details
 from daq_config_server.app import main as main_app
@@ -18,27 +16,6 @@ from tests.constants import TEST_CONFIG_PATH
 def test_cli_version():
     cmd = [sys.executable, "-m", "daq_config_server", "--version"]
     assert subprocess.check_output(cmd).decode().strip() == __version__
-
-
-@patch("daq_config_server.app.main")
-@patch("daq_config_server.__main__.print")
-@patch("daq_config_server.__main__.ArgumentParser.parse_args")
-@patch.dict("sys.modules", {"uvicorn": None, "fastapi": None})
-def test_print_and_exit_if_incorrect_dependencies(
-    mock_parse_args: MagicMock, mock_print: MagicMock, mock_main: MagicMock
-):
-    main()
-    mock_print.assert_called_once_with(INSUFFICIENT_DEPENDENCIES_MESSAGE)
-    mock_main.assert_not_called()
-
-
-@patch("daq_config_server.app.main")
-@patch("daq_config_server.__main__.ArgumentParser.parse_args")
-def test_main_runs_with_correct_dependencies(
-    mock_parse_args: MagicMock, mock_main: MagicMock
-):
-    main()
-    mock_main.assert_called_once()
 
 
 @patch("daq_config_server.app.uvicorn.run")
