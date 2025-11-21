bdcb4deee1865f4a567e005fe77f906ec641fb19
Ollie Silvester <oliver.silvester@diamond.ac.uk>
2025-11-21 14:20:49 +0000

    Rework whitelist tests

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from unittest.mock import patch
 
 import pytest
@@ -5,7 +6,8 @@ from pytest import FixtureRequest
 
 from daq_config_server.testing._utils import make_test_response
 from daq_config_server.whitelist import get_whitelist
-from tests.constants import TEST_WHITELIST_RESPONSE
+
+WHITELIST_PATH = Path(__file__).resolve().parents[2] / "whitelist.yaml"
 
 
 @pytest.fixture(autouse=True)
@@ -13,7 +15,9 @@ def test_friendly_whitelist(request: FixtureRequest):
     # Don't launch threads unless we really need to since it slows down testing
 
     with patch("daq_config_server.whitelist.requests.get") as mock_request:
-        mock_request.return_value = make_test_response(content=TEST_WHITELIST_RESPONSE)
+        with open(WHITELIST_PATH) as f:
+            whitelist_contents = f.read()
+        mock_request.return_value = make_test_response(content=whitelist_contents)
 
         if request.node.get_closest_marker("use_threading"):  # type: ignore
             with patch("daq_config_server.whitelist.WHITELIST_REFRESH_RATE_S", new=0):
