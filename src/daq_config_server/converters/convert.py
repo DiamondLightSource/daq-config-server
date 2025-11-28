c4657bedfe559cc889fbb38e9b963de0dcee4f0c
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2025-11-28 15:32:45 +0000

    Allow requests for pydantic models

--- src/daq_config_server/converters/convert.py
+++ src/daq_config_server/converters/convert.py
@@ -1,17 +1,21 @@
 import json
 from pathlib import Path
-from typing import Any
+
+from pydantic import BaseModel
 
 import daq_config_server.converters._file_converter_map as file_converter_map
 from daq_config_server.converters._converter_utils import ConverterParseError
 
 
-def get_converted_file_contents(file_path: Path) -> Any:
+def get_converted_file_contents(file_path: Path) -> dict:
     with file_path.open("r", encoding="utf-8") as f:
         raw_contents = f.read()
     if converter := file_converter_map.FILE_TO_CONVERTER_MAP.get(str(file_path)):
         try:
-            return converter(raw_contents)
+            contents = converter(raw_contents)
+            if isinstance(contents, BaseModel):
+                return contents.model_dump()
+            return contents
         except Exception as e:
             raise ConverterParseError(
                 f"Unable to parse {str(file_path)} due to the following exception: \
