822a173f3beda35dcc397e64cd8e8f536d71b342
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2025-12-05 15:54:30 +0000

    Add models to public API

---
+++ src/daq_config_server/models/__init__.py
@@ -0,0 +1,7 @@
+from daq_config_server.converters.display_config.models import (
+    DisplayConfig,
+    DisplayConfigData,
+)
+from daq_config_server.converters.lookup_tables.models import GenericLookupTable
+
+__all__ = ["DisplayConfig", "DisplayConfigData", "GenericLookupTable"]
