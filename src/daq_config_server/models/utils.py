1079c60a6a1e5c6c258b553370bd6b14d4af76bc
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-03-12 11:11:16 +0000

    Only use parse_value for beamline parameters

--- src/daq_config_server/models/utils.py
+++ src/daq_config_server/models/utils.py
@@ -1,7 +1,5 @@
-import ast
 import re
 from collections.abc import Iterable
-from typing import Any
 
 
 def remove_comments(lines: Iterable[str]) -> list[str]:
@@ -10,23 +8,6 @@ def remove_comments(lines: Iterable[str]) -> list[str]:
         for line in lines
         if line.strip().split("#", 1)[0]
     ]
-
-
-DEFAULT_REPLACEMENTS = {"Yes": "True", "No": "False", "true": "True", "false": "False"}
-
-
-def parse_value(
-    value: str,
-    convert_to: type | None = None,
-    replacements: dict[str, str] = DEFAULT_REPLACEMENTS,
-) -> Any:
-    """Convert a string value into an appropriate Python type. Optionally provide a type
-    to convert to. If not given, the type will be inferred.
-    """
-    value = ast.literal_eval(replacements.get(value, value))
-    if convert_to:
-        value = convert_to(value)
-    return value
 
 
 def camel_to_snake_case(value: str) -> str:
--- tests/unit_tests/models/test_utils.py
+++ tests/unit_tests/models/test_utils.py
@@ -1,10 +1,7 @@
-from typing import Any
-
 import pytest
 
 from daq_config_server.models.utils import (
     camel_to_snake_case,
-    parse_value,
     remove_comments,
 )
 
@@ -26,23 +23,6 @@ def test_remove_comments_works_as_expected():
         "whitespace should be stripped",
     ]
     assert remove_comments(input) == expected_output
-
-
-@pytest.mark.parametrize(
-    "value, convert_to, expected_parsed_value",
-    [
-        ("  2.0   ", None, 2.0),
-        (" 3 ", None, 3),
-        ("5.0", float, 5.0),
-        ("5.0", int, 5),
-    ],
-)
-def test_parse_value_works_as_expected(
-    value: str, convert_to: type, expected_parsed_value: Any
-):
-    parsed_value = parse_value(value, convert_to)
-    assert parsed_value == expected_parsed_value
-    assert type(parsed_value) is type(expected_parsed_value)
 
 
 @pytest.mark.parametrize(
