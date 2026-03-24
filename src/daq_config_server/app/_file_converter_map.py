35e804b52257286f3007081f683bb5739415b07d
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-03-24 17:49:35 +0000

    Add config file to whitelist and converter map

--- src/daq_config_server/app/_file_converter_map.py
+++ src/daq_config_server/app/_file_converter_map.py
@@ -47,4 +47,5 @@ FILE_TO_CONVERTER_MAP: dict[str, Callable[[str], ConfigModel | dict[str, Any]]] 
     "/dls_sw/i09-1/software/gda/workspace_git/gda-diamond.git/configurations/i09-1-shared/lookupTables/IIDCalibrationTable.txt": parse_i09_hu_undulator_energy_gap_lut,  # noqa: E501
     "/dls_sw/i04/software/daq_configuration/domain/domain.properties": I04FeatureSettings.from_domain_properties,  # noqa: E501
     "/dls_sw/i03/software/daq_configuration/domain/domain.properties": HyperionFeatureSettings.from_domain_properties,  # noqa: E501
+    "/dls_sw/i15-1/software/gda_var/xpdfLocalParameters.xml": xmltodict.parse,
 }
