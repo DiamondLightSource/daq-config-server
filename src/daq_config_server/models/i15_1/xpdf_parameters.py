7237104abe21287ebc72ea0bc88af8d338b9e478
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-03-26 13:28:22 +0000

    PR comments

--- src/daq_config_server/models/i15_1/xpdf_parameters.py
+++ src/daq_config_server/models/i15_1/xpdf_parameters.py
@@ -26,13 +26,8 @@ class TemperatureControllersConfig(ConfigModel):
     @classmethod
     def from_xpdf_parameters(cls, contents: str) -> Self:
         config_dict = xmltodict.parse(contents)
-        cobra = TemperatureControllerParams.model_validate(
-            config_dict["configuration"]["devices"]["cobra"]
-        )
-        blower = TemperatureControllerParams.model_validate(
-            config_dict["configuration"]["devices"]["blower"]
-        )
-        cryostream = TemperatureControllerParams.model_validate(
-            config_dict["configuration"]["devices"]["cryostream"]
-        )
+        devices = config_dict["configuration"]["devices"]
+        cobra = TemperatureControllerParams.model_validate(devices["cobra"])
+        blower = TemperatureControllerParams.model_validate(devices["blower"])
+        cryostream = TemperatureControllerParams.model_validate(devices["cryostream"])
         return cls(cobra=cobra, blower=blower, cryostream=cryostream)
