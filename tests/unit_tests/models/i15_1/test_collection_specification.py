f86d2820491be9454b7f66c319290eb4af15c210
Dominic Oram <dominic.oram@diamond.ac.uk>
2026-09-11 16:41:55 +0000

    Add option for fast attenuation in data collection

--- tests/unit_tests/models/i15_1/test_collection_specification.py
+++ tests/unit_tests/models/i15_1/test_collection_specification.py
@@ -7,18 +7,42 @@ from daq_config_server.models.i15_1.collection_specification import (
 )
 
 
-def test_collection_spec_parses_json_contents():
+def test_collection_spec_parses_lut_contents():
     with open(TestDataPaths.TEST_I15_1_POSITIONS_TIMES_LUT) as f:
         contents = f.read()
 
     result = CollectionSpecification.from_lut(contents)
     assert result.tth_angle_to_specification == {
-        10: SpecificationPerPosition(exposure_time=0.05, transmission=0.01),
-        20: SpecificationPerPosition(exposure_time=0.05, transmission=0.1),
-        30: SpecificationPerPosition(exposure_time=0.1, transmission=1),
-        40: SpecificationPerPosition(exposure_time=0.2, transmission=10),
-        50: SpecificationPerPosition(exposure_time=0.3, transmission=50),
-        60: SpecificationPerPosition(exposure_time=0.3, transmission=100),
+        10: SpecificationPerPosition(
+            exposure_time=0.05,
+            slow_attenuator_transmission=0.01,
+            fast_attenuator_position="IN",
+        ),
+        20: SpecificationPerPosition(
+            exposure_time=0.05,
+            slow_attenuator_transmission=0.1,
+            fast_attenuator_position="IN",
+        ),
+        30: SpecificationPerPosition(
+            exposure_time=0.1,
+            slow_attenuator_transmission=1,
+            fast_attenuator_position="OUT",
+        ),
+        40: SpecificationPerPosition(
+            exposure_time=0.2,
+            slow_attenuator_transmission=10,
+            fast_attenuator_position="OUT",
+        ),
+        50: SpecificationPerPosition(
+            exposure_time=0.3,
+            slow_attenuator_transmission=50,
+            fast_attenuator_position="OUT",
+        ),
+        60: SpecificationPerPosition(
+            exposure_time=0.3,
+            slow_attenuator_transmission=100,
+            fast_attenuator_position="OUT",
+        ),
     }
 
 
@@ -26,12 +50,36 @@ def test_collection_spec_normalises_times():
     result = CollectionSpecification.model_validate(
         {
             "tth_angle_to_specification": {
-                10: {"exposure_time": 1, "transmission": 0.01},
-                20: {"exposure_time": 1, "transmission": 0.01},
-                30: {"exposure_time": 2, "transmission": 0.01},
-                40: {"exposure_time": 4, "transmission": 0.01},
-                50: {"exposure_time": 6, "transmission": 0.01},
-                60: {"exposure_time": 6, "transmission": 0.01},
+                10: {
+                    "exposure_time": 1,
+                    "slow_attenuator_transmission": 0.01,
+                    "fast_attenuator_position": "IN",
+                },
+                20: {
+                    "exposure_time": 1,
+                    "slow_attenuator_transmission": 0.01,
+                    "fast_attenuator_position": "IN",
+                },
+                30: {
+                    "exposure_time": 2,
+                    "slow_attenuator_transmission": 0.01,
+                    "fast_attenuator_position": "OUT",
+                },
+                40: {
+                    "exposure_time": 4,
+                    "slow_attenuator_transmission": 0.01,
+                    "fast_attenuator_position": "OUT",
+                },
+                50: {
+                    "exposure_time": 6,
+                    "slow_attenuator_transmission": 0.01,
+                    "fast_attenuator_position": "OUT",
+                },
+                60: {
+                    "exposure_time": 6,
+                    "slow_attenuator_transmission": 0.01,
+                    "fast_attenuator_position": "OUT",
+                },
             }
         }
     )
